76500e0cfd7ea7e43a11d20f245958d4983911c2
Eric <ericnguyen23@gmail.com>
2023-03-20 15:29:16 -0700

    Created triangle svg, added switch to check what shape, added text variable

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const Circle = require("./lib/circle.js");
+const Triangle = require("./lib/triangle.js");
 
-inquirer.prompt([{ name: "color", message: "SVG color:" }]).then((data) => {
-  const newCircle = new Circle();
-  styledCircle = newCircle.render(data.color);
+inquirer
+  .prompt([
+    { name: "color", message: "SVG color:" },
+    { name: "shape", message: "SVG shape:" },
+    { name: "text", message: "SVG text (max 3 chars):" },
+  ])
+  .then((data) => {
+    switch (data.shape) {
+      case "circle":
+        const newCircle = new Circle();
+        styledShape = newCircle.render(data.color, data.text);
+        break;
+      case "triangle":
+        const newTriangle = new Triangle();
+        styledShape = newTriangle.render(data.color, data.text);
+        break;
+    }
 
-  fs.writeFile("./output/logo.svg", styledCircle, (err) => {
-    err ? console.log(err) : console.log("Success!");
+    fs.writeFile("./output/logo.svg", styledShape, (err) => {
+      err ? console.log(err) : console.log("Success!");
+    });
   });
-});
