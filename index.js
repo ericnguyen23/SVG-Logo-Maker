9189947625b6054c8536fea9f3ebc59f42160ca9
Eric <ericnguyen23@gmail.com>
2023-03-20 15:13:49 -0700

    Created ouput folder to store saved svg

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 const Circle = require("./lib/circle.js");
 
 inquirer.prompt([{ name: "color", message: "SVG color:" }]).then((data) => {
   const newCircle = new Circle();
-  console.log(newCircle.render(data.color));
+  styledCircle = newCircle.render(data.color);
+
+  fs.writeFile("./output/logo.svg", styledCircle, (err) => {
+    err ? console.log(err) : console.log("Success!");
+  });
 });
